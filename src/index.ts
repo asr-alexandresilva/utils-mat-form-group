324b471003f0b6548f2f3abf8e2eb6b131472581
asr-alexandresilva <arpsdesigner@gmail.com>
2023-06-11 17:07:23 -0300

    fix: adjustment export class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
-export * from "./utils-mat-form-group.module";
+import { FormGroup, FormControl } from "@angular/forms";
+
+class UtilsMatFormGroup {
+  constructor() {}
+
+  public validateFormGroup(form: FormGroup) {
+    let validField = true;
+    Object.entries(form.controls).forEach(([, val]) => {
+      let valItemForm = val as FormControl;
+      if (valItemForm.status == "INVALID") {
+        validField = false;
+        valItemForm.markAsTouched();
+      }
+    });
+
+    return validField;
+  }
+
+  insertValuesFormGroup(dataObject: any, form: FormGroup) {
+    try {
+      Object.entries(dataObject).forEach(([key, val]) => {
+        if (typeof val === "boolean") {
+          form.controls[`${key}`].setValue(val);
+        } else {
+          form.controls[`${key}`].setValue(`${val !== null ? val : ""}`);
+        }
+      });
+    } catch (error) {
+      console.warn("Error insert data fom form group");
+    }
+  }
+}
+
+export default UtilsMatFormGroup;
